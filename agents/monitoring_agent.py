7cc79a4f5e499f5079e3eb7d0dda882b0a430f51
jasmee-n <jnjasmee2017@gmail.com>
2026-08-24 19:51:24 +0100

    update

--- agents/monitoring_agent.py
+++ agents/monitoring_agent.py
@@ -12,7 +12,7 @@ class MonitoringAgent:
         self.clinical_knowledge = clinical_knowledge
 
     @traceable(name = 'Monitoring Agent', run_type = 'chain')
-    def run(self, state: SMRState):
+    def run(self, state):
 
         bnf_tables = self.clinical_knowledge.retrieve_bnf_tables(state.medications.medications)
             
